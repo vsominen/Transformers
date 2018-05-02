10ca6e2a0d7d8f013d8f6103eefc919264c26a79
kanchanahegde <kanchanaghegde@gmail.com>
2018-05-02 10:21:10 -0400

    Initial setup

---
+++ avengers/src/test/java/com/studio/lab/avengers/CaseChangeTransformTest.java
@@ -0,0 +1,19 @@
+package com.studio.lab.avengers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CaseChangeTransformTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
