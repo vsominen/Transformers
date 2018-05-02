e915505eff81205865edb87b588410e5ef2a6380
kanchanahegde <kanchanaghegde@gmail.com>
2018-05-02 10:41:05 -0400

    updated all test cases

--- avengers/src/test/java/com/studio/lab/avengers/CaseChangeTransformTest.java
+++ avengers/src/test/java/com/studio/lab/avengers/CaseChangeTransformTest.java
@@ -6,14 +6,40 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CaseChangeTransformTest {
-
+    private CaseChangeTransform caseTransform;
     @Before
     public void setUp() throws Exception {
+        caseTransform = new CaseChangeTransform();
     }
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testCaseChangeMixedCaseShouldYieldReversedCase() {
+        String str = "ChangeCase";
+        assertEquals("cHANGEcASE", caseTransform.caseChange(str));
+    }
+    
+    @Test
+    public void testCaseChangeNonAlphaCharactersShouldYieldNoChange() {
+        String str = "123ChangeCase 123";
+        assertEquals("123cHANGEcASE 123", caseTransform.caseChange(str));
+    }
+
+    @Test
+    public void testCaseChangeNullCharactersShouldYieldNull() {
+        String str = null;
+        assertEquals(null, caseTransform.caseChange(str));
+    }
+    
+    @Test
+    public void testForwardTranformCharactersShouldYieldReversedCase() {
+        String str = "123ChangeCase 123";
+        assertEquals("123cHANGEcASE 123", caseTransform.forwardTransform(str));
+    }
+    
+    @Test
+    public void testReverseTranformCharactersShouldYieldReversedCase() {
+        String str = "123cHANGEcASE 123";
+        assertEquals("123ChangeCase 123", caseTransform.reverseTransform(str));
     }
 
 }
